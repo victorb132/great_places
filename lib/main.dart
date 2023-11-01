0aa8c988af9e23b226583e207593b072b55effa1
Victor Novais <victor96novais@gmail.com>
2023-11-01 08:06:17 -0300

    feat/place-form

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/screens/place_from_screen.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+import 'package:great_places/utils/app.routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,61 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Great Places',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        hintColor: Colors.amber,
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const PlacesListScreen(),
+      routes: {
+        AppRoutes.placeForm: (ctx) => const PlaceFormScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/place_from_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/widgets/image_input.dart';
+
+class PlaceFormScreen extends StatefulWidget {
+  const PlaceFormScreen({super.key});
+
+  @override
+  State<PlaceFormScreen> createState() => _PlaceFormScreenState();
+}
+
+class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  void _submitForm() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Novo lugar'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: _submitForm,
+            icon: const Icon(Icons.add),
+            label: const Text('Adicionar'),
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              backgroundColor: Theme.of(context).hintColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
